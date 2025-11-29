0557b5dcdc36fbe435c8214ca388457dd5bae0f1
Tadbir20 <tadbirtareqg2@gmail.com>
2025-11-29 21:38:44 +0600

    by MURSALIN

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 import 'package:cafetrack/login_screen.dart';
-
+// this is main .dart
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
